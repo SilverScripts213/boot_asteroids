2b6aa5fd780013046b10dfcf3005cdb396aa3c16
SilverScripts213 <SilverSister213@gmail.com>
2024-12-08 11:37:45 -0600

    It's alive!

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import pygame
-from constants.py import *
+from constants import *
 
-def __main__():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+def main():
+    pygame.init
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        screen.fill("black")
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
 
 if __name__ == "__main__":
     main()
