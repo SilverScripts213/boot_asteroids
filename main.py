a230656f3bd98d1603a6730b2937cd038d2c8ea9
SilverScripts213 <SilverSister213@gmail.com>
2024-12-08 12:16:48 -0600

    The ship does the thing

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = PLAYER(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
